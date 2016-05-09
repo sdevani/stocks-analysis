2b524b0abaf74d0e38a25331ef7575d18a566949
Shehzan Devani <shehzandevani@gmail.com>
2016-05-08 23:01:31 -0500

    Moving things into tempHelper

--- scripts/strategies/comboPeriodicTrends.js
+++ scripts/strategies/comboPeriodicTrends.js
@@ -1,40 +1,4 @@
-var db = require('../../db.js');
-var Stock = db.Stock;
-var _ = require('underscore');
-
-var DAYS_TO_HOLD = 5;
-var MINIMUM_WIN = 2;
-
-
-var results = {};
-var strategyCount = 1;
-var addStrategyToResult = function(strategyFunction) {
-  var name = 'Strategy ' + String(strategyCount++);
-  addNamedStrategyToResult(name, strategyFunction);
-};
-
-var addNamedStrategyToResult = function(name, strategyFunction) {
-  results[name] = {
-    buyTrigger: 0,
-    profitableDays: 0,
-    profits: 0,
-    positiveSum: 0,
-    negativeSum: 0,
-    strategyFunction: strategyFunction
-  };
-};
-
-var createYearBasedStrategy = function(year, func) {
-  return function(stock, stocks, i) {
-    return stock.date.getFullYear() == year && func(stock, stocks, i);
-  };
-}
-
-var generateStrategyPerYear = function(func) {
-  for (var year = 2000; year < 2017; year++) {
-    addStrategyToResult(createYearBasedStrategy(year, func));
-  }
-};
+var tempHelper = require('./services/tempHelper.js');
 
 var standardStrategy = function(stock, stocks, i) {
   var yearlyDelta = 10;
@@ -47,7 +11,7 @@ var standardStrategy = function(stock, stocks, i) {
     Math.abs(stock.oneDayDelta + dailyDeltaSize) < dailyDeltaSize
     && stock.date.getFullYear() != 2003;
 };
-addStrategyToResult(standardStrategy);
+tempHelper.addStrategyToResult(standardStrategy);
 
 var highToLowStrategy = function() {
   return function(stock, stocks, i) {
@@ -138,60 +102,4 @@ var categoryStrategy = function(year, quarter, month, week, day) {
 //   return stock.oneWeekDelta < -3 && stock.oneDayDelta > 1;
 // });
 
-Stock.findAll({
-  order: [['date', 'ASC']]
-}).then(function(stocks) {
-  return _.groupBy(stocks, function(stock) { return stock.ticker; });
-}).then(function(stockByTicker) {
-  for (var ticker in stockByTicker) {
-    var stocks = stockByTicker[ticker];
-    for (var i = 1; i < stocks.length - 1 - DAYS_TO_HOLD; i++) {
-      var stock = stocks[i];
-      // var stockYear = stock.date.getFullYear();
-      var stocksBought = 10000/(stocks[i+1].open);
-      var profit = stocksBought * stocks[i+DAYS_TO_HOLD].close - 10000;
-      if (isNaN(profit) || profit < -5000 || profit > 5000) { continue; }
-      for (var strategy in results) {
-        var strategyFunction = results[strategy].strategyFunction;
-        if (strategyFunction(stock, stocks, i)) {
-          addResult(results[strategy], profit);
-        }
-      }
-    }
-  }
-}).then(function() {
-  printResults(results);
-});
-
-var addResult = function(results, profit) {
-  results.buyTrigger++;
-  results.profits += profit;
-  if (profit > 0) {
-    results.profitableDays++;
-    results.positiveSum += profit;
-  } else {
-    results.negativeSum += profit;
-  }
-}
-
-var printResults = function(results) {
-  for (var type in results) {
-    console.log("STRATEGY: ", type);
-    var typeResult = results[type];
-    console.log(formatResults(typeResult));
-    console.log("\n\n");
-  }
-};
-
-var formatResults = function(results) {
-  // # of triggers
-  // % profitable
-  // $/trigger
-  if (!results) { return ""; }
-  if (results.buyTrigger === 0) { return "Triggers: " + results.buyTrigger; }
-  return "Triggers: " + results.buyTrigger +
-    "\tProfitability %: " + (results.profitableDays / results.buyTrigger * 100.0) +
-    "\tProfits/Day: " + results.profits/results.buyTrigger +
-    "\tNegative/Day:" + results.negativeSum/results.buyTrigger + 
-    "\tPositive/Day:" + results.positiveSum/results.buyTrigger;
-}
+tempHelper.run();
---
+++ scripts/strategies/services/tempHelper.js
@@ -0,0 +1,128 @@
+var db = require('../../../db.js');
+var Stock = db.Stock;
+var _ = require('underscore');
+
+var DAYS_TO_HOLD = 1;
+var MINIMUM_WIN = 2;
+
+var results = {};
+
+var strategyCount = 1;
+var addStrategyToResult = function(strategyFunction) {
+  var name = 'Strategy ' + String(strategyCount++);
+  addNamedStrategyToResult(name, strategyFunction);
+};
+
+var createYearBasedStrategy = function(year, func) {
+  return function(stock, stocks, i) {
+    return stock.date.getFullYear() == year && func(stock, stocks, i);
+  };
+}
+
+var generateStrategyPerYear = function(func) {
+  for (var year = 2000; year < 2017; year++) {
+    addStrategyToResult(createYearBasedStrategy(year, func));
+  }
+};
+
+var addNamedStrategyToResult = function(name, strategyFunction) {
+  results[name] = {
+    buyTrigger: 0,
+    profitableDays: 0,
+    profits: 0,
+    positiveSum: 0,
+    negativeSum: 0,
+    strategyFunction: strategyFunction
+  };
+};
+
+var addResult = function(results, profit) {
+  results.buyTrigger++;
+  results.profits += profit;
+  if (profit > 0) {
+    results.profitableDays++;
+    results.positiveSum += profit;
+  } else {
+    results.negativeSum += profit;
+  }
+};
+
+var formatResults = function(results) {
+  // # of triggers
+  // % profitable
+  // $/trigger
+  if (!results) { return ""; }
+  if (results.buyTrigger === 0) { return "Triggers: " + results.buyTrigger; }
+  return "Triggers: " + results.buyTrigger +
+    "\tProfitability %: " + (results.profitableDays / results.buyTrigger * 100.0) +
+    "\tProfits/Day: " + results.profits/results.buyTrigger +
+    "\tNegative/Day:" + results.negativeSum/results.buyTrigger + 
+    "\tPositive/Day:" + results.positiveSum/results.buyTrigger;
+};
+
+var printResults = function(results) {
+  for (var type in results) {
+    console.log("STRATEGY: ", type);
+    var typeResult = results[type];
+    console.log(formatResults(typeResult));
+    console.log("\n\n");
+  }
+};
+
+var standardSell = function(daysToHold, buyAt, sellAt) {
+  return function(stocks, i) {
+    var stocksBought = 100/stocks[i + 1][buyAt];
+    var profit = stocks[i + 1 + daysToHold][sellAt] * stocksBought - 100;
+    return profit;
+  };
+};
+
+var earlySell = function(daysToHold, buyAt, sellAt, target) {
+  var backupSell = standardSell(daysToHold, buyAt, sellAt);
+  return function(stocks, i) {
+    var stocksBought = 100/stocks[i + 1][buyAt];
+    var targetPrice = (100 + target)/stocksBought;
+    var firstDay = i + 1;
+    var lastDay = i + daysToHold + 1;
+    if (sellAt === 'open') { lastDay--; }
+    for (var day = firstDay; day <= lastDay; day++) {
+      if (stocks[day].high > targetPrice) { return target; }
+    }
+    return backupSell(stocks, i);
+  }
+}
+
+var run = function() {
+  // var sellFunction = standardSell(DAYS_TO_HOLD, 'open', 'close');
+  Stock.findAll({
+    order: [['date', 'ASC']]
+  }).then(function(stocks) {
+    return _.groupBy(stocks, function(stock) { return stock.ticker; });
+  }).then(function(stockByTicker) {
+    for (var ticker in stockByTicker) {
+      var stocks = stockByTicker[ticker];
+      for (var i = 1; i < stocks.length - 1 - DAYS_TO_HOLD; i++) {
+        var stock = stocks[i];
+        // var stockYear = stock.date.getFullYear();
+        var stocksBought = 10000/(stocks[i+1].open);
+        var profit = stocksBought * stocks[i+DAYS_TO_HOLD].close - 10000;
+        if (isNaN(profit) || profit < -5000 || profit > 5000) { continue; }
+        for (var strategy in results) {
+          var strategyFunction = results[strategy].strategyFunction;
+          if (strategyFunction(stock, stocks, i)) {
+            addResult(results[strategy], profit);
+          }
+        }
+      }
+    }
+  }).then(function() {
+    printResults(results);
+  });
+}
+
+module.exports = {
+  run: run,
+  addStrategyToResult: addStrategyToResult,
+  addNamedStrategyToResult: addNamedStrategyToResult,
+  generateStrategyPerYear: generateStrategyPerYear
+};
